ab63ff1afdf804646bf26e83a48898fa7f9d594d
Anthony Alcorn <xmastony@gmail.com>
2020-08-19 02:20:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -67,7 +67,7 @@ def game_hash
     },
     :away => {
       :team_name => 'Charlotte Hornets',
-      :colors => ['turquoise', 'purple'],
+      :colors => ['Turquoise', 'Purple'],
       :players => [
         {
         player_name: 'Jeff Adrien', 
