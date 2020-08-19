3e1a072fcf5358589507dcefb0effb921ff28ce4
Anthony Alcorn <xmastony@gmail.com>
2020-08-19 02:32:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,7 +5,7 @@ def game_hash
   game_hash = {
     :home => {
       :team_name => 'Brooklyn Nets',
-      :colors => ['black', 'white'],
+      :colors => ['Black', 'White'],
       :players => [
         {player_name: 'Alan Anderson', 
         number: 0, 
